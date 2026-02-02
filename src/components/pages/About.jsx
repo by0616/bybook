a907954bbf1e31ec2ca505cb90d6717319648ac1
by0616 <baoyong0616@outlook.com>
2026-02-02 16:02:46 +0800

    optimize NavBar codes

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Welcome to the About page. Here I share my story.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/pages/Blog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Welcome to the Blog page. Here we share our latest news and articles.</p>
+    </div>
+  );
+}
+
+export default Blog;
