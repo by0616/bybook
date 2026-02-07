112d18352ccd44dee9146c72f75500c1bedacb24
by0616 <baoyong0616@outlook.com>
2026-02-07 21:30:08 +0800

    Home page update

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 
 const Home = () => {
+    const [showIntro, setShowIntro] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setShowIntro(window.scrollY > 40);
+        };
+
+        handleScroll();
+        window.addEventListener("scroll", handleScroll, { passive: true });
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
         <div className="HomeHero">
             <div className="HomeCover">
@@ -12,6 +24,10 @@ const Home = () => {
             </div>
             <div className="HomeTitleWrap">
                 <h1 className="PageTitle HomeTitle">bybook</h1>
+                <p className={`HomeIntro ${showIntro ? "is-visible" : ""}`}>
+                    Welcome to bybook, my personal website. Click the Navigator on the right
+                    to see the different pages of the website. To be updated!
+                </p>
             </div>
         </div>
     );
