6d8b72e3efc26607e01d8189f2ecb1d0a729e9e6
by0616 <baoyong0616@outlook.com>
2026-02-07 20:29:43 +0800

    Home page optimizing

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>
-            <h1 className="PageTitle">bybook</h1>
-            <hr className="HomeLine" />
+        <div className="HomeHero">
+            <div className="HomeCover">
+                <span className="HomeArc HomeArc--one" />
+                <span className="HomeArc HomeArc--two" />
+                <span className="HomeLine HomeLine--one" />
+                <span className="HomeLine HomeLine--two" />
+            </div>
+            <div className="HomeTitleWrap">
+                <h1 className="PageTitle HomeTitle">bybook</h1>
+            </div>
         </div>
     );
 }
